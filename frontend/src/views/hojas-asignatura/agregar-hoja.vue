7f3e6850e9c082756cb036ed74f4d443ba6dce08
Azael <bryan.lopez.21m@utzmg.edu.mx>
2023-11-03 08:54:53 -0600

    Hoja de asignatura v.0

---
+++ frontend/src/views/hojas-asignatura/agregar-hoja.vue
@@ -0,0 +1,34 @@
+<template>
+    <headerComponent></headerComponent>
+    <body>
+      <label id="labelC">Carrera</label>
+      <input>
+      <label>Materia</label>
+      <table></table>
+    </body>
+
+</template>
+
+<script>
+
+import headerComponent from '@/components/header-component.vue';
+
+export default {
+  name: "SubjectSheetsAdd",
+  components: {headerComponent},
+
+  setup() {
+    
+
+    return{
+      
+    }
+    
+  },
+
+
+};
+//Investigar para que sirve ref en vue
+</script>
+
+<style src="@/assets/css/style.css"></style>
