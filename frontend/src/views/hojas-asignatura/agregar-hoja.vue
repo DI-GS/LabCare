6329e762fc7f7957e9f2b1208c444646ebb9f58c
Azael <bryan.lopez.21m@utzmg.edu.mx>
2023-11-05 11:29:55 -0600

    Hoja de asignatura v.0.1

--- frontend/src/views/hojas-asignatura/agregar-hoja.vue
+++ frontend/src/views/hojas-asignatura/agregar-hoja.vue
@@ -1,12 +1,51 @@
 <template>
+  <body>
+  <div class="main-container">
     <headerComponent></headerComponent>
-    <body>
-      <label id="labelC">Carrera</label>
-      <input>
-      <label>Materia</label>
-      <table></table>
-    </body>
 
+    <div class="content">
+      <label id="labelC">Carrera</label>
+        <select v-model="selectedOption">
+          <option value="" disabled selected>Selecciona una opción</option>
+          <option value="opcion1">Opción 1</option>
+          <option value="opcion2">Opción 2</option>
+          <option value="opcion3">Opción 3</option>
+        </select>
+      <label>Materia</label>
+        <select v-model="selectedOption">
+          <option value="" disabled selected>Selecciona una opción</option>
+          <option value="opcion1">Opción 1</option>
+          <option value="opcion2">Opción 2</option>
+          <option value="opcion3">Opción 3</option>
+        </select>
+    </div>
+    <div>
+      <label>Unidad temática</label>
+        <div>
+          <label for="">Número</label>
+          <select name="NumberSheets" id="NumberSheets">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          </select>
+        </div>
+        <div>
+          <label for="nameSubject">Nombre</label>
+            <input type="text">
+        </div>
+        <div>
+          <label for="themes">Temas</label>
+            <input type="text">
+            <button class="cross-button">+</button>
+
+        </div>
+    </div>
+  </div>
+  </body>
+  
 </template>
 
 <script>
