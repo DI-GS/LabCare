6b977eb72bb5ff69a88b0ce2afd02911d52ebf81
Azael <bryan.lopez.21m@utzmg.edu.mx>
2023-11-25 18:13:02 -0600

    Objeto que guarda los temas y la información de la hoja de asignatura

--- frontend/src/views/hojas-asignatura/subjectSheetsAdd.vue
+++ frontend/src/views/hojas-asignatura/subjectSheetsAdd.vue
@@ -7,15 +7,15 @@
         <div class="row">
           <div class="col-md-6 mb-3">
           <label for="carrera">Carrera</label>
-          <select class="form-control" v-model="selectedCarrera">
-            <!-- Opciones de carrera -->
+          <select class="form-control" id="careerSelected">
+            <option value="IDGS">IDGS</option>
           </select>
         </div>
 
         <div class="col-md-6 mb-3">
           <label for="materia">Materia</label>
-          <select class="form-control" v-model="selectedMateria">
-            <!-- Opciones de materia -->
+          <select class="form-control" id="subjectSelected">
+            <option value="BD">BD</option>
           </select>
         </div>
         </div>
@@ -27,36 +27,46 @@
           <div class="col-6 mb-3">
             <label for="numero">No. Unidad</label>
             <select class="form-control" name="NumberSheets" id="NumberSheets">
-              <!-- Opciones para número de hojas -->
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
             </select>
           </div>
 
           <div class="col-6 mb-3">
             <label for="nameSubject">Nombre</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" id="NameSheets">
           </div>
         </div>
 
         <div class="row">
           <div class="col-6 mb-3">
             <label for="numero">Horas teoricas</label>
-            <select class="form-control" name="NumberSheets" id="NumberSheets">
-              <!-- Opciones para número de hojas -->
-            </select>
+            <input type="number" class="form-control" name="NumberSheets" id="HourTSheets">
           </div>
 
           <div class="col-6 mb-3">
             <label for="nameSubject">Horas practicas</label>
-            <input type="text" class="form-control">
+            <input type="number" class="form-control" id="HourPSheets">
           </div>
         </div>
         
         <label for="temas">Temas</label>
         <div class="input-group mb-3">
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" id="ThemeSheets">
           <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button">+</button>
+            <button @click="agregarTema" class="btn btn-outline-secondary btn-color" type="button">+</button>
           </div>
+        </div>
+        <div class="cuadroVisualizacion form-group" id="subjectRecolect">
+          <ul>
+            <li v-for="(theme, index) in newTheme.themes" :key="index">
+              {{ theme.theme }}
+            </li>
+      </ul>
         </div>
       </div>
     </div>
@@ -69,15 +79,54 @@
 import headerComponent from '@/components/header-component.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 export default {
   name: "SubjectSheetsAdd",
   components: {headerComponent},
+  
+  
 
   setup() {
-    
+    let newTheme = {
+      subjet: "",
+      career: "",
+      NameSheets: "",
+      NumberSheets:"",
+      themes: []
+    };
+
+    const agregarTema = function () {
+      const career = document.getElementById('careerSelected').value;
+      const subject = document.getElementById('subjectSelected').value;
+      const numberSheets = document.getElementById('NumberSheets').value;
+      const nameSheets = document.getElementById('NameSheets').value;
+      const hourTSheets = document.getElementById('HourTSheets').value;
+      const hourPSheets = document.getElementById('HourPSheets').value;
+      const themeSheets = document.getElementById('ThemeSheets').value;
+
+      const nuevoTema = {  
+        theme: themeSheets,
+        hourTeoSheets: hourTSheets,
+        hourPracSheets: hourPSheets,
+      };
+
+      newTheme.NameSheets = nameSheets;
+      newTheme.NumberSheets= numberSheets;
+      newTheme.career = career;
+      newTheme.subjet = subject;
+      newTheme.themes.push(nuevoTema);
+
+      document.getElementById('HourTSheets').value = '';
+      document.getElementById('HourPSheets').value = '';
+      document.getElementById('ThemeSheets').value = '';
+      
+      console.log('Temas después de agregar:', newTheme.themes[0].theme);
+      console.log('Nuevo tema:', this.newTheme);
+    };
 
     return{
-      
+        newTheme,
+        agregarTema
     }
     
   },
